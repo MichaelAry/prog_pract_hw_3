309125c1ef2a9f63a90213d68241081dccc9b5bf
Michael <orimihail2006@gmail.com>
2024-11-21 23:41:08 +0300

    Update 1.6.cpp

--- 1.6.cpp
+++ 1.6.cpp
@@ -1,24 +1,40 @@
-#include <iostream> 
+#include <iostream>
 #include <cmath>
+float ta{0};
+int tn{0}; 
 
-void enterFunc(float af, int nf);
-float a{0}; int n{0};
+void enterValue(float af, int nf);
 
-int main() {
-    enterFunc(0,0);
-    std::cout<<a;std::cout<<n;
-    return 0; 
+int main(){
+    float a{0};
+    int n{0};
+    a=ta;
+    n=tn;
+
+    enterValue(0,0);
+    a=ta;
+    n=tn;
+
+    return 0;
 }
 
-void enterFunc(float af, int nf) {
-    while(1)/*&&(IsInteger(n))*/{
-        std::cout << "Please enter a number: ";
-        std::cin>>af; std::cout<<std::endl;
-        std::cout << "Please enter a natural power: ";
-        std::cin>>nf; std::cout<<std::endl;
-        if ((nf>=0)&&(nf==round(nf))){break;}
-        else {continue;}
+void enterValue(float af, int nf){
+    while (1){
+        std::cout<<"Enter the number: ";
+        std::cin>>af; 
+        std::cout<<std::endl;
+        ta=af;
+        
+        std::cout<<"Enter the natural power: ";
+        std::cin>>nf; 
+        std::cout<<std::endl;     
+        tn=nf;
+
+        if ((nf>=0)&&(nf==round(nf))){
+            break;
+        }
+        else{
+            continue; 
+        }
     }
-    a=af;
-    n=nf;
 }
