76d1f8f7b11fe70d5c64babf2263f71d7bf1f340
Michael <orimihail2006@gmail.com>
2024-11-21 22:34:58 +0300

    "Added function enterFunc to prompt user for float and int input, and modified main function to call enterFunc and print the input values."

--- 1.6.cpp
+++ 1.6.cpp
@@ -1,7 +1,24 @@
 #include <iostream> 
+#include <cmath>
+
+void enterFunc(float af, int nf);
+float a{0}; int n{0};
 
 int main() {
-
-
+    enterFunc(0,0);
+    std::cout<<a;std::cout<<n;
     return 0; 
+}
+
+void enterFunc(float af, int nf) {
+    while(1)/*&&(IsInteger(n))*/{
+        std::cout << "Please enter a number: ";
+        std::cin>>af; std::cout<<std::endl;
+        std::cout << "Please enter a natural power: ";
+        std::cin>>nf; std::cout<<std::endl;
+        if ((nf>=0)&&(nf==round(nf))){break;}
+        else {continue;}
+    }
+    a=af;
+    n=nf;
 }
