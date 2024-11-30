c737a9461d770186b6a6e0870a1044576a3d8387
Michael <67581497+MichaelAry@users.noreply.github.com>
2024-11-30 12:17:25 +0300

    Add files via upload

--- 1.6.cpp
+++ 1.6.cpp
@@ -1,40 +1,42 @@
 #include <iostream>
 #include <cmath>
-float ta{0};
-int tn{0}; 
 
-void enterValue(float af, int nf);
+int enterValue();
+float power(float a, int p);
 
 int main(){
     float a{0};
-    int n{0};
-    a=ta;
-    n=tn;
+    int p{0};
 
-    enterValue(0,0);
-    a=ta;
-    n=tn;
+    std::cout<<"Enter any number: ";
+    std::cin>>a;
+    std::cout<<std::endl;
 
+    p=enterValue();
+    std::cout <<a<<" in power "<<p<<" = "<<power(a,p);
     return 0;
 }
 
-void enterValue(float af, int nf){
+int enterValue(){
+    float t{0};
     while (1){
-        std::cout<<"Enter the number: ";
-        std::cin>>af; 
-        std::cout<<std::endl;
-        ta=af;
-        
         std::cout<<"Enter the natural power: ";
-        std::cin>>nf; 
-        std::cout<<std::endl;     
-        tn=nf;
-
-        if ((nf>=0)&&(nf==round(nf))){
+        std::cin>>t; 
+        std::cout<<std::endl;
+        if ((t>=0)&&(t==round(t))){
             break;
         }
         else{
             continue; 
         }
     }
+    return t;
+}
+
+float power(float a, int p){
+    float res{1};
+    if(p>=1){
+        res=power(a,p-1)*a;
+    }
+    return res;
 }
