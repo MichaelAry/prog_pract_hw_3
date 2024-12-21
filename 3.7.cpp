a9990124f78bc1c502fe8c0b15ce32885d5bb38d
Michael <67581497+MichaelAry@users.noreply.github.com>
2024-12-21 09:55:06 +0300

    Update 3.7.cpp

--- 3.7.cpp
+++ 3.7.cpp
@@ -1,7 +1,23 @@
-#include <iostream> 
+#include <iostream>
+#include <cmath>
 
-int main() {
+void funcVal();
 
+int main(){
+    funcVal();
+    return 0;
+}
 
-    return 0; 
+void funcVal(){
+    float sum{0};
+    float x{0.5};
+    while (x<=2){
+        sum=0;
+        for(int n{1};n<1000;n++){
+            sum+=pow((x-1),n)/(n*pow(x,n));
+        }
+        std::cout<<"x="<<x<<"   "<<" res="<<sum/logl(10)<<std::endl;
+        x+=0.05;
+
+    }    
 }
